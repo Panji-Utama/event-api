834a199e21e91d00f4eb832af4be1faebde637e4
FIRMAN PANJI <fpu2468@gmail.com>
2024-02-07 00:18:36 +0700

    feat: Added .env loading, Event Model migration, and connection to ElephantSQL(Postgres)

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/Panji-Utama/event-api/models"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// Run before main, or you could just insert it at main
+func init() {
+	// Load .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(("Error loading .env file"))
+	}
+
+	// Connect to DB
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	
+	db.AutoMigrate(&models.Event{})
+}
 
 func main() {
-	fmt.Println("First Commit")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // Default :8080, take PORT from .env
+
 }
