f08bce059500644509c8ea55e7ad00257636e22b
FIRMAN PANJI <fpu2468@gmail.com>
2024-02-07 23:29:58 +0700

    feat: Post Events route

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/Panji-Utama/event-api/controllers"
 	"github.com/Panji-Utama/event-api/models"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -11,7 +12,8 @@ import (
 	"gorm.io/gorm"
 )
 
-// Run before main, or you could just insert it at main
+var db *gorm.DB
+
 func init() {
 	// Load .env
 	err := godotenv.Load()
@@ -21,19 +23,37 @@ func init() {
 
 	// Connect to DB
 	dsn := os.Getenv("DATABASE_URL")
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// Use assignment operator = instead of :=
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
 
-	
-	db.AutoMigrate(&models.Event{})
+	// Automigrate models
+	err = db.AutoMigrate(&models.Event{})
+	if err != nil {
+		log.Fatalf("Error automigrating models: %v", err)
+	}
 }
 
 func main() {
 	r := gin.Default()
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "ping pong hey",
 		})
 	})
-	r.Run() // Default :8080, take PORT from .env
 
+	r.GET("/events", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ping pong hey",
+		})
+	})
+
+	r.POST("/events", func(c *gin.Context) {
+		controllers.EventsCreate(c, db) // Pass db variable to EventsCreate
+	})
+	
+	r.Run(":" + os.Getenv("PORT")) // Default :8080, take PORT from .env
 }
